5f9aad0c0a16b33f1940da1f2bda02352b639e9c
Harsh <shahmharsh@gmail.com>
2017-08-12 18:37:16 -0700

    Tableview setup

--- chocolate/ViewController.swift
+++ chocolate/ViewController.swift
@@ -8,9 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    let cellIdentifier = "chocolateCellIdentifier"
+    
+    var chocolates = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        chocolates = ["Five Star", "Dairy Milk", "Some other one"]
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chocolates.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let chocolate = chocolates[indexPath.row]
+        cell.textLabel?.text = chocolate
+        return cell
     }
 }
